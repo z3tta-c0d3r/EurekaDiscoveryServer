736dddfbc8a62eea061d1d72ed741adf44d1495a
vicente.garcia <vicente.garcia@outlook.com>
2020-11-17 12:27:47 +0100

    First commit

---
+++ src/test/java/com/zeta/microservices/discovery/eurekadiscoveryserver/EurekaDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeta.microservices.discovery.eurekadiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
